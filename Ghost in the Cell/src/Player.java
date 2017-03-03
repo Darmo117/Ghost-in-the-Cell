f6c429fd9c11dc2a2005e839b47c3541053a8481
Darmo <darmo117@gmail.com>
2017-03-03 13:12:04 +0100

    feat : màj

--- Ghost in the Cell/src/Player.java
+++ Ghost in the Cell/src/Player.java
@@ -58,14 +58,27 @@ class Player {
             o.nb = 0;
             d.nb -= toSend;
           }
-          
-          
-            actions.add(move(o.factory.id, d.factory.id, toSend));
+          actions.add(move(o.factory.id, d.factory.id, toSend));
+          o.factory.virtualStock -= toSend;
+          d.factory.virtualStock += toSend;
           if (d.nb == 0) break;
-                       
         }
       }
-
+      
+      Query[] attackUnits = Arrays.stream(offers)
+        .filter(q -> q.nb > 0)
+        .toArray(Query[]::new);
+      Factory[] targets = factories.stream()
+        .filter(f -> f.owner != 1)
+        .sorted((f1, f2) -> f1.virtualStock - f2.virtualStock)
+        .toArray(Factory[]::new);
+        
+      for (Factory f : targets) {
+          for (Query q : attackUnits) {
+              
+          }
+      }
+      
       System.out.println(actions);
     }
   }
@@ -103,14 +116,19 @@ class Player {
 
       if (entityType.equals("FACTORY")) {
         Factory f = factories.get(entityId);
+        int diff = f.virtualStock - f.stock;
 
         f.owner = arg1;
         f.stock = arg2;
         f.prod = arg3;
-        f.virtualStock = arg2;
+        f.virtualStock = arg2 + diff;
       }
     }
   }
+    
+    private static String move(int src, int dest, int nb) {
+        return String.format("MOVE %d %d %d", src, dest, nb);
+    }
 }
 
 class Factory {
